014f67302f81cf7aec84b6f3eb295d29e5cc31b3
Aunuxx <132584420+Aunuxx@users.noreply.github.com>
2024-10-10 10:57:09 -0600

    Update Paragraph.java

--- project7/src/Paragraph.java
+++ project7/src/Paragraph.java
@@ -1,3 +1,26 @@
+// Paragraph: This file contains the class Paragraph.
+    // It contains the following private data member:
+        // ArrayList of Sentences – a list of all the sentences in this Paragraph.
+    // It contains the following methods:
+        // There is no constructor – the default constructors will be sufficient.
+        // void addASentence(String theText) – This method adds a Sentence to the ArrayList of Sentences.
+        // void print() – calls print() for every Sentence in the ArrayList of Sentences.
+
+import java.util.List;
+import java.util.ArrayList;
+
 public class Paragraph {
-    
+    private List<String> sentences = new ArrayList<>();
+
+    public void addASentence(String theText) {
+        sentences.add(theText);
+    }
+
+    public void print() {
+        for (int i = 0; i < sentences.size(); i++) {
+            System.out.printf("%s ", sentences.get(i));
+        }
+        System.out.println();
+    }
+
 }
