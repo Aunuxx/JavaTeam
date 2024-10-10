158679dfba85b467c99abbd34f32cb0818b9384a
Aunuxx <132584420+Aunuxx@users.noreply.github.com>
2024-10-10 11:02:36 -0600

    Update Sentence.java

--- project7/src/Sentence.java
+++ project7/src/Sentence.java
@@ -1,3 +1,20 @@
+// Sentence: This file contains the class Sentence.
+    // It contains the following private data member:
+        // String theSentence – storage for a single sentence.
+    // It contains the following methods:
+        // Sentence(String inputSentence) – a one element (one parameter) constructor. It simply
+        // sets theSentence with the in-coming sentence.
+        // void print(String theText) – This method simply prints theSentence.
+
 public class Sentence {
-    
+    private String theSentence;
+
+    public Sentence(String inputSentence) {
+        theSentence = inputSentence;
+    }
+
+    public void print() {
+        System.out.println(theSentence);
+    }
+
 }
