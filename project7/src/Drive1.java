0522a98651a3f4f89d93372e7c7b4add1a2c83dd
justinrwoo <justinwoolsey118@gmail.com>
2024-10-13 18:37:18 -0600

    Create Drive1.java

---
+++ project7/src/Drive1.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class Drive {
+
+    public static void printMenu() {
+        System.out.println("This program allows a user to enter a written composition in pieces: ");
+        System.out.println("1. Enter a new sentence.");
+        System.out.println("2. Start a new paragraph.");
+        System.out.println("3. Print the composition.");
+        System.out.println("4. Quit.");
+        System.out.print("Your menu selection is: ");
+    }
+
+    public static void main(String[] args) {
+        Scanner scnr = new Scanner(System.in);
+        Composition comp = new Composition();
+        boolean done = false;
+
+        while (!done) {
+            printMenu();
+            int menuChoice = scnr.nextInt();
+
+            if (menuChoice == 1) {
+                System.out.println("Type a sentence, then press ENTER;");
+                String sentence = scnr.next();
+                comp.addASentence(sentence);
+            }
+            else if (menuChoice == 2) {
+                comp.addAParagraph();
+            }
+            else if (menuChoice == 3) {
+                comp.print();
+            }
+            else if (menuChoice == 4) {
+                done = true;
+            }
+        }
+    }
+}
