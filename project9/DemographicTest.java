71ab643e801a852ee6a70125a470461f2f19d129
justinrwoo <justinwoolsey118@gmail.com>
2024-11-03 21:58:52 -0700

    done

--- project9/DemographicTest.java
+++ project9/DemographicTest.java
@@ -1,5 +1,9 @@
 package project9;
 
 public class DemographicTest {
-    
+    public static void main(String[] args) {
+        DemographicLL demographicList = new DemographicLL();
+        demographicList.printList();
+    }
 }
+
