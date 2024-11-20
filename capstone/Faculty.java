1e55df56cabfa51ba21cd8158c293d17ebb4ee9b
justinrwoo <justinwoolsey118@gmail.com>
2024-11-20 06:39:27 -0700

    no errors

--- capstone/Faculty.java
+++ capstone/Faculty.java
@@ -17,48 +17,16 @@ public class Faculty extends CollegeEmployee  {
         Scanner scnr = new Scanner(System.in);
         String uIn;
 
-        System.out.print("First name: ");
-        uIn = scnr.next();
-        fname = uIn;
-
-        System.out.print("Last name: ");
-        uIn = scnr.next();
-        lname = uIn;
-
-        System.out.print("Address: ");
-        uIn = scnr.next();
-        addr = uIn;
-
-        System.out.print("Zip code: ");
-        uIn = scnr.next();
-        zip = Integer.valueOf(uIn);
-
-        System.out.print("Phone number: ");
-        uIn = scnr.next();
-        phoneNumber = Integer.valueOf(uIn);
-
-
-        System.out.print("Social Security number: ");
-        uIn = scnr.next();
-        ssn = uIn;
-
-        System.out.print("Salary: ");
-        uIn = scnr.next();
-        salary = Integer.valueOf(uIn);
-
-        System.out.print("Department: ");
-        uIn = scnr.next();
-        department = uIn;
+        super.input();
 
         System.out.print("Tenured: ");
         uIn = scnr.next();
         tenured = Boolean.valueOf(uIn);
 
-        scnr.close();
     }
 
     public void printInfo() {
-        System.out.printf("%s %s %s %d %d %d %d %s %b%n",
-            fname, lname, addr, zip, phoneNumber, ssn, salary, department, tenured);
+        System.out.printf("%s %s %s %d %d %s %d %s %b%n",
+                fname, lname, addr, zip, phoneNumber, ssn, salary, department, tenured);
     }
 }
